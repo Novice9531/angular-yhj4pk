79a3a0c2f769525dfc900107acc8e81e70dd7293
Novice9531 <marshall.k@outlook.co.nz>
2022-06-29 11:39:51 +1200

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,91 @@
 import { Component } from '@angular/core';
-
+ 
+import { MenuItemModel } from '@syncfusion/ej2-angular-navigations';
+ 
+ 
+ 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ 
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+ 
 })
-export class AppComponent { }
+ 
+export class AppComponent {
+ 
+public menuItems: MenuItemModel[] = [
+ 
+    {
+ 
+        text: 'File',
+ 
+        items: [
+ 
+            {text: 'Open'},
+ 
+            {text: 'Save'},
+ 
+            {text: 'Exit'}
+ 
+        ]
+ 
+    },
+ 
+    {
+ 
+        text: 'Edit',
+ 
+        items: [
+ 
+            {text: 'Cut'},
+ 
+            {text: 'Copy'},
+ 
+            {text: 'Paste'}
+ 
+        ]
+ 
+    },
+ 
+    {
+ 
+        text: 'View',
+ 
+        items: [
+ 
+            {text: 'Toolbar'},
+ 
+            {text: 'Sidebar'}
+ 
+        ]
+ 
+    },
+ 
+    {
+ 
+        text: 'Tools',
+ 
+        items: [
+ 
+            {text: 'Spelling & Grammar'},
+ 
+            {text: 'Customize'},
+ 
+            {text: 'Options'}
+ 
+        ]
+ 
+    },
+ 
+       {text: 'Go'},
+ 
+       {text: 'Help'}
+ 
+];
+ 
+}
+ 
 
 
 /*
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { MenuModule } from '@syncfusion/ej2-angular-navigations';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { enableRipple } from '@syncfusion/ej2-base';
+
+enableRipple(true);
 
 @NgModule({
   imports: [
     BrowserModule,
-    
+    MenuModule,
     CommonModule,
     FormsModule,
     
